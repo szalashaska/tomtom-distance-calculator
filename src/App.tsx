e83859db86e0bfbe92abe043d8201682308e8545
szalashaska <kamilpetryniak@gmail.com>
2023-02-18 21:42:54 +0100

    Added marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import {
   ChangeEvent,
   Dispatch,
-  FormEvent,
   SetStateAction,
   useEffect,
   useRef,
   useState,
 } from "react";
 import * as tt from "@tomtom-international/web-sdk-maps";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import "./App.scss";
 
 const API_KEY = import.meta.env.VITE_TOMOTOM_API_KEY;
@@ -18,14 +18,33 @@ function App() {
   const [longitude, setLongitude] = useState<number>(-0.112869);
   const [latitude, setLatitude] = useState<number>(51.504);
 
-  const handleUserInput = (
+  const handleUserInput: (
     e: ChangeEvent<HTMLInputElement>,
     setAction: Dispatch<SetStateAction<number>>
-  ) => {
+  ) => void = (e, setAction) => {
     const userInput = +e.target.value;
     if (isNaN(userInput) || userInput < -90 || userInput > 90) return;
     setAction(userInput);
   };
+
+  const addMarker = () => {
+    if (!map) return;
+    const element = document.createElement("div");
+    element.className = "marker";
+    const marker = new tt.Marker({
+      draggable: true,
+      element: element,
+    })
+      .setLngLat([longitude, latitude])
+      .addTo(map);
+
+    marker.on("dragend", () => {
+      const lngLat = marker.getLngLat();
+      setLatitude(lngLat.lat);
+      setLongitude(lngLat.lng);
+    });
+  };
+  addMarker();
 
   useEffect(() => {
     let mapObject: tt.Map;
@@ -43,43 +62,43 @@ function App() {
       mapObject = tt.map(mapOptions);
       setMap(mapObject);
     }
-
     return () => {
-      setMap(null);
-      mapObject.remove();
+      if (mapObject) mapObject.remove();
     };
   }, [longitude, latitude]);
 
   return (
-    <div className="App">
-      <div className="map" ref={mapRef} />
-      <div className="searchbar">
-        <h1>Where to?</h1>
-        <label htmlFor="latitude">Latitude:</label>
-        <input
-          value={latitude}
-          type="text"
-          id="latitude"
-          className="latitude"
-          placeholder="Latitude..."
-          onChange={(e) => {
-            handleUserInput(e, setLatitude);
-          }}
-        />
-        <label htmlFor="longitude">Longitude:</label>
+    <>
+      <div className="App">
+        <div className="map" ref={mapRef} />
+        <div className="searchbar">
+          <h1>Where to?</h1>
+          <label htmlFor="latitude">Latitude:</label>
+          <input
+            value={latitude}
+            type="text"
+            id="latitude"
+            className="latitude"
+            placeholder="Latitude..."
+            onChange={(e) => {
+              handleUserInput(e, setLatitude);
+            }}
+          />
+          <label htmlFor="longitude">Longitude:</label>
 
-        <input
-          value={longitude}
-          type="text"
-          id="longitude"
-          className="longitude"
-          placeholder="Longitude..."
-          onChange={(e) => {
-            handleUserInput(e, setLongitude);
-          }}
-        />
+          <input
+            value={longitude}
+            type="text"
+            id="longitude"
+            className="longitude"
+            placeholder="Longitude..."
+            onChange={(e) => {
+              handleUserInput(e, setLongitude);
+            }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
