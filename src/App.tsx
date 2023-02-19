3e8e81bb9e8fdd98c822ce15b7ded180dac8022d
szalashaska <kamilpetryniak@gmail.com>
2023-02-19 20:14:30 +0100

    Added route calculations

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,25 @@ import {
   useState,
 } from "react";
 import * as tt from "@tomtom-international/web-sdk-maps";
+import * as ttAPi from "@tomtom-international/web-sdk-services";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import "./App.scss";
+
+type ApiResponseType = {
+  response: {
+    routeSummary: {
+      travelTimeInSeconds: number;
+    };
+  };
+};
+
+type ResultsType = {
+  location: tt.LngLat;
+  drivingtime: number;
+};
+
+// location: destinationRef.current[idx],
+// drivingtime: item.response.routeSummary.travelTimeInSeconds,
 
 const API_KEY = import.meta.env.VITE_TOMOTOM_API_KEY;
 
@@ -17,6 +34,7 @@ function App() {
   const [map, setMap] = useState<tt.Map | null>(null);
   const [longitude, setLongitude] = useState<number>(-0.112869);
   const [latitude, setLatitude] = useState<number>(51.504);
+  const destinationRef = useRef<tt.LngLat[]>([]);
 
   const handleUserInput: (
     e: ChangeEvent<HTMLInputElement>,
@@ -27,24 +45,130 @@ function App() {
     setAction(userInput);
   };
 
-  const addMarker = () => {
+  const createPopup: () => tt.Popup = () => {
+    const popupOffset: { bottom: [number, number] } = {
+      bottom: [0, -45],
+    };
+    const popup = new tt.Popup({
+      offset: popupOffset,
+    }).setHTML("This is you!");
+    return popup;
+  };
+
+  const addMarker: (map: tt.Map) => void = (map) => {
     if (!map) return;
     const element = document.createElement("div");
-    element.className = "marker";
+    element.className = "driver-marker";
     const marker = new tt.Marker({
       draggable: true,
-      element: element,
+      element,
     })
       .setLngLat([longitude, latitude])
       .addTo(map);
+
+    const popup = createPopup();
 
     marker.on("dragend", () => {
       const lngLat = marker.getLngLat();
       setLatitude(lngLat.lat);
       setLongitude(lngLat.lng);
     });
+
+    marker.setPopup(popup).togglePopup();
   };
-  addMarker();
+
+  const convertToPoints: (lngLat: tt.LngLat) => {
+    point: {
+      latitude: number;
+      longitude: number;
+    };
+  } = (lngLat) => {
+    // const point = {
+    //   latitude: lngLat.lat,
+    //   longitude: lngLat.lng,
+    // };
+    return {
+      point: {
+        latitude: lngLat.lat,
+        longitude: lngLat.lng,
+      },
+    };
+  };
+
+  const addDeliveryMarker: (lngLat: tt.LngLat, map: tt.Map) => void = (
+    lngLat,
+    map
+  ) => {
+    const element = document.createElement("div");
+    element.className = "delivery-marker";
+    new tt.Marker({ element }).setLngLat(lngLat).addTo(map);
+  };
+
+  const convertArray = (destinations: tt.LngLat[]) => {
+    return destinations.map((item) => convertToPoints(item));
+  };
+
+  const sortDestenations: (
+    destinations: tt.LngLat[]
+  ) => Promise<tt.LngLat[]> = (destinations) => {
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    };
+
+    const callParams: ttAPi.MatrixRoutingOptions = {
+      key: API_KEY,
+      destinations: convertArray(destinations),
+      origins: [convertToPoints(origin as tt.LngLat)],
+    };
+
+    return new Promise((resolve, reject) => {
+      ttAPi.services.matrixRouting(callParams).then((matrixApiResults) => {
+        const results = matrixApiResults.matrix[0] as ApiResponseType[];
+        const resultsArray: ResultsType[] = results.map((item, idx) => ({
+          location: destinations[idx],
+          drivingtime: item.response.routeSummary.travelTimeInSeconds,
+        }));
+        const sortedLocation = resultsArray
+          .sort((a, b) => a.drivingtime - b.drivingtime)
+          .map((item) => item.location);
+
+        resolve(sortedLocation);
+      });
+    });
+  };
+
+  const drawRoute = (geoJson: tt.GeoJSONFeature, map: tt.Map) => {
+    if (map.getLayer("route")) {
+      map.removeLayer("route");
+      map.removeSource("route");
+    }
+    map.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: geoJson,
+      },
+      paint: {
+        "line-color": "red",
+        "line-width": 6,
+      },
+    });
+  };
+
+  const recalculateToutes = (map: tt.Map) => {
+    sortDestenations(destinationRef.current).then((sorted) => {
+      sorted.unshift({ lng: longitude, lat: latitude } as tt.LngLat);
+
+      ttAPi.services
+        .calculateRoute({ key: API_KEY, locations: sorted })
+        .then((routeData) => {
+          const geoJson = routeData.toGeoJson() as unknown;
+          drawRoute(geoJson as tt.GeoJSONFeature, map);
+        });
+    });
+  };
 
   useEffect(() => {
     let mapObject: tt.Map;
@@ -60,6 +184,15 @@ function App() {
         },
       };
       mapObject = tt.map(mapOptions);
+      addMarker(mapObject);
+
+      mapObject.on("click", (e: tt.MapMouseEvent<"click">) => {
+        if (destinationRef.current) {
+          destinationRef.current.push(e.lngLat);
+          addDeliveryMarker(e.lngLat, mapObject);
+          recalculateToutes(mapObject);
+        }
+      });
       setMap(mapObject);
     }
     return () => {
@@ -69,7 +202,7 @@ function App() {
 
   return (
     <>
-      <div className="App">
+      <div className="app">
         <div className="map" ref={mapRef} />
         <div className="searchbar">
           <h1>Where to?</h1>
--- src/helpers/types.ts
+++
@@ -1,4 +0,0 @@
-export type TomTomType = {
-  key: string;
-  container: HTMLElement;
-};
